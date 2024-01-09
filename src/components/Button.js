ee65a3f576b3a912b485a991457d738fb59597f7
martinromario55 <martinromario55@gmail.com>
2024-01-09 23:28:04 +0300

    Removed redundant imports

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export const Button = ({children}) => {
   return (
       <button className="w-64 text-xl text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 rounded-lg px-5 py-2.5 mr-2 mb-2 font-medium hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Link, NavLink, useNavigate } from 'react-router-dom'
 
 import Logo from '../assets/mlogo.jpg'
